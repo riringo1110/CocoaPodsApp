a1269b5c77b8de55e9eeecc2a6520c0d26a1c5cf
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-14 01:09:23 +0900

    CocoaPods!

--- CocoaPodsApp/ViewController.swift
+++ CocoaPodsApp/ViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import PKHUD
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //viewDidAppearのタイミングでHUDの表示
+        HUD.flash(.success, delay: 2.0)
+    }
 }
+
 
